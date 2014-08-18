f5dd0378a4f8e772160434a15c09326fa471084d
Stuart Berg <bergs@janelia.hhmi.org>
2014-08-18 12:49:58 -0400

    Added ilastik_python.sh

---
+++ ilastik_python.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# we assume that this script resides in BUILDEM_DIR
+export BUILDEM_DIR=$(cd `dirname $0` && pwd)
+
+# This script is intended for use within the fully packaged binary.
+# Therefore, we want our environment to use ONLY the libraries 
+#  included in our own lib directory, not the user's environment
+export ILASTIK_USE_CLEAN_LD_LIBRARY_PATH=1
+
+source $BUILDEM_DIR/bin/setenv_ilastik_gui.sh
+$BUILDEM_DIR/bin/python2.7 "$@"
